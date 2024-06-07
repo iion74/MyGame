381903125898a529a1eff9080da91ad862b88118
iion74 <147688919+iion74@users.noreply.github.com>
2024-06-07 22:40:47 +0900

    launcher 실행

--- Assets/Script/launcher.cs
+++ Assets/Script/launcher.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class launcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float launcherSpeed = 1f; // launcher의 이동속도
+    private Vector3 playerPos = new Vector3(0f, -3.5f, 0f); // launcher의 초기 위치
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        // launcher의 좌우이동
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal")* launcherSpeed);
+        // launcher의 이동제한
+        playerPos = new Vector3(Mathf.Clamp(xPos,-2f,2f), -3.5f, 0f);
+        transform.position = playerPos;
     }
 }
